5813d050da24d3437546b2a95f2e3466e7579d54
Aldoreino <alberto.abraham.castillo@gmail.com>
2015-05-19 09:56:52 -0700

    sets the routes constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	# defines the api
+	namespace :api, defaults: { format: :json },
+								constraints: { subdomain: 'api' } path: '/' do # sets the starting path in rails to be in root in relation to the subdomain
+		# the resources will be placed in here
 
+	end
 end
